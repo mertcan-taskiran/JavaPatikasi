c3e232a79b51bece4e0f8ecfb316af064e9d3165
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-05-07 22:42:54 +0300

    Kullanici Girisi Eklendi

--- src/main/java/Pratik/KullaniciGirisi.java
+++ src/main/java/Pratik/KullaniciGirisi.java
@@ -8,12 +8,15 @@ public class KullaniciGirisi {
 
         Scanner inp = new Scanner(System.in);
 
+        // Kullanıcı adı gir
         System.out.print("Kullanıcı Adınız: ");
         userName = inp.nextLine();
 
+        // Şifre gir
         System.out.print("Şifreniz: ");
         password = inp.nextLine();
 
+        // Kontrol ediliyor
         if (userName.equals("patika") && password.equals("java")) {
             System.out.println("Giriş Başarılı");
         } else {
@@ -24,12 +27,14 @@ public class KullaniciGirisi {
 
             if (reset.equals("E")) {
 
+                // Yeni şifre girişi
                 System.out.print("Yeni Şifrenizi Giriniz: ");
                 newPassword = inp.nextLine();
 
                 if (!newPassword.equals(password)) {
                     System.out.println("Şifre oluşturuldu");
                 } else {
+                    // Eski şifre kullanılamaz
                     System.out.println("Hata: Yeni şifre eski şifreyle aynı olamaz.");
                     System.out.print("Başka bir şifre giriniz: ");
                     newPassword = inp.nextLine();
