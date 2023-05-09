458e88f9a2a33fe6e4f792a0bf018eb341730270
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-05-09 13:08:28 +0300

    Elmas Yıldız Yazdırma

---
+++ src/main/java/Pratik/Yildizlar.java
@@ -0,0 +1,31 @@
+package Pratik;
+import java.util.Scanner;
+public class Yildizlar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int n = scanner.nextInt();
+
+        // Üst kısım
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Alt kısım
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
