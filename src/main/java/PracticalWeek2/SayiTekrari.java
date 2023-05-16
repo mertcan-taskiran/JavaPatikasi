27a020956d73d43c6497711da06bd6f8cdef4420
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-05-16 13:59:13 +0300

    Tekrar eden çift sayılar

---
+++ src/main/java/PracticalWeek2/SayiTekrari.java
@@ -0,0 +1,40 @@
+package PracticalWeek2;
+
+import java.util.Arrays;
+
+public class SayiTekrari {
+    static boolean isFind(int[] arr, int value){
+        for (int i:arr)
+        {
+            if (i == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] list = {3,4,1,5,7,8,3,2,4,6,6,9,9};
+        int[] duplicate = new int[list.length];
+        int startIndex = 0;
+        for (int i = 0; i<list.length; i++)
+        {
+            for (int j = 0; j<list.length; j++)
+            {
+                if ((i != j) && (list[i] == list[j]) &&j%2==0)
+                {
+                    if (list[i] % 2 == 0 && !isFind(duplicate, list[i]))
+                    {
+                        duplicate[startIndex++] = list[i];
+                    }
+                    break;
+                }
+            }
+        }
+        for (int value: duplicate)
+        {
+            if (value != 0){
+                System.out.println(value);
+            }
+        }
+    }
+}
